2d41b3da6dd2821f73658b6564d95fc5dc860f26
Matheus-dSouza <matheus.dsouza@sptech.school>
2025-11-28 13:20:54 -0300

    Correção de navegação entre telas de técnico

--- Site Institucional/src/routes/Jira.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const visualizarChamadosJira = require("../controllers/JiraController");
-
-router.get("/visualizarChamadosJira", function (req, res) {
-    visualizarChamadosJira(req, res);
-})
-
-module.exports = router;
