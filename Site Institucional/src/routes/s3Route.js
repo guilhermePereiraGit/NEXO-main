8b57c342f5ae28bd6cf3dd5cf74b9297063b0942
Matheus-dSouza <matheus.dsouza@sptech.school>
2025-11-29 16:51:52 -0300

    Corrigindo KPIs implementadas

--- Site Institucional/src/routes/s3Route.js
+++ Site Institucional/src/routes/s3Route.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-
 var s3Controller = require('../controllers/s3Controller');
 
-router.get('/dados/:diretorio/:conteudo', (req, res) => {
-    s3Controller.lerArquivo(req, res);
+router.get('/dados/*', (req, res) => {  // Captura tudo após /dados/
+  s3Controller.lerArquivo(req, res);
 });
 
 module.exports = router;
