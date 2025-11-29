2097bd72f96e36cae84a153306a9c6fa25c2afc2
Matheus-dSouza <matheus.dsouza@sptech.school>
2025-11-29 16:25:38 -0300

    Corrigindo KPIs implementadas

--- Site Institucional/src/controllers/s3Controller.js
+++ Site Institucional/src/controllers/s3Controller.js
@@ -1,36 +1,29 @@
 const AWS = require('aws-sdk');
-const Papa = require('papaparse');
+const { json } = require('express');
+
 AWS.config.update({ region: process.env.AWS_REGION });
 const s3 = new AWS.S3();
 
 async function lerArquivo(req, res) {
   try {
-    const fileKey = req.params[0];  // Pega o path completo após /dados/
-    if (!fileKey || !/^[\w.\-/]+$/.test(fileKey)) {  // Valida com / permitido
-      return res.status(400).json({ erro: 'Nome de arquivo inválido.' });
-    }
+    const fileKey = req.params.diretorio + "/" + req.params.conteudo;
     const params = {
       Bucket: process.env.S3_BUCKET,
       Key: fileKey
     };
+
+    //Conferindo o que está sendo lendo
     console.log(`📥 Lendo do S3: ${params.Bucket}/${params.Key}`);
+
     const data = await s3.getObject(params).promise();
     const text = data.Body.toString('utf-8').trim();
-    let content;
-    if (text.startsWith('[') || text.startsWith('{')) {
-      content = JSON.parse(text);
-    } else {
-      const parsed = Papa.parse(text, {
-        header: true,
-        delimiter: text.includes(';') ? ';' : ',',
-        skipEmptyLines: true
-      });
-      content = parsed.data;
-    }
-    res.json(content);
+
+    //Convertendo o text para um arquivo Json
+    res.json(JSON.parse(text));
+
   } catch (err) {
     console.error('❌ Erro ao buscar arquivo:', err.message);
-    res.status(500).json({ erro: 'Erro ao buscar arquivo: ' + err.message });
+    res.status(500).send('Erro ao buscar arquivo: ' + err.message);
   }
 }
 
