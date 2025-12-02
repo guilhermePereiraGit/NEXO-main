eb52ee7fdd09597db2fb30b995b33733e50113fe
Matheus-dSouza <matheus.dsouza@sptech.school>
2025-12-01 23:14:32 -0300

    Readicionando a função lerArquivo no s3Controller

--- Site Institucional/src/controllers/s3Controller.js
+++ Site Institucional/src/controllers/s3Controller.js
@@ -3,6 +3,29 @@ const Papa = require('papaparse');
 AWS.config.update({ region: process.env.AWS_REGION });
 const s3 = new AWS.S3();
 
+async function lerArquivo(req, res) {
+  try {
+    const fileKey = req.params.diretorio + "/" + req.params.conteudo;
+    const params = {
+      Bucket: process.env.S3_BUCKET,
+      Key: fileKey
+    };
+
+    //Conferindo o que está sendo lendo
+    console.log(`📥 Lendo do S3: ${params.Bucket}/${params.Key}`);
+
+    const data = await s3.getObject(params).promise();
+    const text = data.Body.toString('utf-8').trim();
+
+    //Convertendo o text para um arquivo Json
+    res.json(JSON.parse(text));
+
+  } catch (err) {
+    console.error('❌ Erro ao buscar arquivo:', err.message);
+    res.status(500).send('Erro ao buscar arquivo: ' + err.message);
+  }
+}
+
 async function lerArquivoMatheus(req, res) {
   try {
     const fileKey = req.params[0];  // Pega o path completo após /dados/
